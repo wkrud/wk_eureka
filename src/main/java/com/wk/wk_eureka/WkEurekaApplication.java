4155932bcda2094ac1d04fdbaf5aff0a0a5aa328
wkrud <99162203+wkrud@users.noreply.github.com>
2024-10-02 04:06:29 +0900

    git test

--- src/main/java/com/wk/wk_eureka/WkEurekaApplication.java
+++ src/main/java/com/wk/wk_eureka/WkEurekaApplication.java
@@ -10,16 +10,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class WkEurekaApplication {
 
-//    @Value("${sever.port}")
-//    private String port;
-
     public static void main(String[] args) {
         SpringApplication.run(WkEurekaApplication.class, args);
     }
-
-//    @PostConstruct
-//    private void start() {
-//        System.out.println("port = " + port);
-//    }
 
 }
